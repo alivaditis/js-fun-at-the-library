ce02dde6315abd7f7ea04f311660b3d5dbf97c03
Alec Livaditis <aleclivaditis@gmail.com>
2023-03-22 18:51:20 -0400

    Add src files for buildMainCharacter and saveReview

--- src/book.js
+++ src/book.js
@@ -2,10 +2,24 @@ function createTitle(title) {
   return `The ${title}`;  
 }
 
+function buildMainCharacter(name, age, pronouns) {
+  mainCharacter = {};
+  mainCharacter.name = name;
+  mainCharacter.age = age;
+  mainCharacter.pronouns = pronouns;
+  return mainCharacter;
+}
+
+function saveReview(reviewContent, reviewArray) {
+  if (reviewArray.includes(reviewContent) === false) {
+  reviewArray.push(reviewContent);
+  }
+}
+
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
+  buildMainCharacter,
+  saveReview,
   // calculatePageCount,
   // writeBook,
   // editBook
--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,20 @@
+function shelfBook(title, shelfName) {
+  if (shelfName.length < 3) {
+    shelfName.unshift(title);
+  }
+}
+
+function unshelfBook(title, shelfName) {
+  for (i=0; i < shelfName.length; i++) {
+    if (shelfName[i] == title) {
+      shelfName.splice(i, 1);
+    }
+  }
+}
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
+  shelfBook,
+  unshelfBook
   // listTitles,
   // searchShelf
 };
