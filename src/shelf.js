5991959f223674d925d60cea0ffebd8ef051d383
Alec Livaditis <aleclivaditis@gmail.com>
2023-03-23 18:07:50 -0400

    add unShelfBook and listTitles functions

--- src/shelf.js
+++ src/shelf.js
@@ -5,16 +5,32 @@ function shelfBook(title, shelfName) {
 }
 
 function unshelfBook(title, shelfName) {
-  for (i=0; i < shelfName.length; i++) {
-    if (shelfName[i] == title) {
-      shelfName.splice(i, 1);
+  for (var i= shelfName.length - 1; i >= 0; i--) {
+    if (shelfName[i].title === title) {
+      shelfName.splice(i,1);
     }
+}
+}
+
+function listTitles(shelfName) {
+  titles = ""
+  for (i = 0; i < shelfName.length; i++) {
+    if (i === shelfName.length -1) {
+      titles = titles + shelfName[i].title
+    } else {
+    titles = titles + shelfName[i].title + ", "
   }
+  }
+  return(titles)
+}
+
+function searchShelf() {
+  
 }
 
 module.exports = {
   shelfBook,
-  unshelfBook
-  // listTitles,
-  // searchShelf
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
