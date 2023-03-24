fc6e2532e00497dd46c4ff6670fbd5b5a3df9a0b
Alec Livaditis <aleclivaditis@gmail.com>
2023-03-24 16:15:51 -0400

    refactor addBook function

--- src/library.js
+++ src/library.js
@@ -10,21 +10,42 @@ function createLibrary(libraryName) {
   return library
 }
 
+// function addBook(library, book) {
+//   if (book.genre === "fantasy") {
+//     library.shelves.fantasy.push(book)
+//   } else if (book.genre === "fiction") {
+//     library.shelves.fiction.push(book) 
+//   } else {
+//     library.shelves.nonFiction.push(book)
+//   }
+//   return library
+// }
+
+// refactored
+
 function addBook(library, book) {
-  library = library
-  if (book.genre === "fantasy") {
-    library.shelves.fantasy.push(book)
-  } else if (book.genre === "fiction") {
-    library.shelves.fiction.push(book) 
-  } else {
-    library.shelves.nonFiction.push(book)
-  }
-  return library
+  library.shelves[book.genre].push(book);
+  return library;
 }
+
+
+function checkoutBook(library, book, shelf) {
+  for ( i = library.shelves[shelf].length - 1; i >= 0; i--) {
+    if (library.shelves[shelf][i].title === book) {
+      library.shelves[shelf].slice(i)
+      console.log(library)
+      return `You have now checked out ${book} from the ${library.name}.`
+    } else {
+       return `Sorry, there are currently no copies of ${book}
+       available at the ${library.name}.`
+          }
+  }
+}
+
 
 module.exports = {
   createLibrary,
   addBook,
-  // checkoutBook,
+  checkoutBook,
   // takeStock
 };
