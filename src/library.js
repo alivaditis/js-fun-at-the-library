dc1a672748a9409fc6e033ba34497db569b76553
Alec Livaditis <aleclivaditis@gmail.com>
2023-03-24 09:38:14 -0400

    add addBook function

--- src/library.js
+++ src/library.js
@@ -1,8 +1,30 @@
+function createLibrary(libraryName) {
+  var library = {
+    name: libraryName,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: []
+    }
+  }
+  return library
+}
 
+function addBook(library, book) {
+  library = library
+  if (book.genre === "fantasy") {
+    library.shelves.fantasy.push(book)
+  } else if (book.genre === "fiction") {
+    library.shelves.fiction.push(book) 
+  } else {
+    library.shelves.nonFiction.push(book)
+  }
+  return library
+}
 
 module.exports = {
-  // createLibrary,
-  // addBook,
+  createLibrary,
+  addBook,
   // checkoutBook,
   // takeStock
 };
